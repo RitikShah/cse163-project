cc9eb409596c4c5938660b915a04a1efa574f4cb
Ritik Shah <ritik10.shah@gmail.com>
2019-05-12 16:00:15 -0700

    rit: NOT WORKING. plz fix

--- internal/process.py
+++ internal/process.py
@@ -3,7 +3,6 @@ import pandas as pd
 import json
 
 
-# Some constants for the file names
 def process(in_name, out_name):
     """
     TODO: better comment
@@ -12,34 +11,32 @@ def process(in_name, out_name):
     dataframe with messages from users in that list, and dataframe only
     contain columns
     """
-    df = pd.read_csv(in_name)
-    df_filtered = df[['fromUser.displayName', 'fromUser.id', 'text']]
-    rank = df_filtered.groupby(['fromUser.displayName', 'fromUser.id']).count()
-    rank = rank[rank['text'] > 10000]
-    sort_rank = rank.sort_values(ascending=False, by='text')
-    # print(sort_rank)  # TODO: use logger
-    name_list = sort_rank.index.tolist()
-    # print(name_list, len(name_list)) # TODO: use logger
-    df = df[df['fromUser.displayName'].isin(name_list)]
-    col_list = [
-        'fromUser.displayName',
-        'fromUser.username',
-        'fromUser.id',
-        'mentions',
-        'urls',
-        'readBy',
-        'editedAt',
-        'sent',
-        'id',
-        'text'
-    ]
-    df = df[col_list]
-    # print(df)  # TODO: use logger
+    try:
+        df = pd.read_csv(in_name)
+        df['text_count'] = \
+            df.groupby(['fromUser.displayName', 'fromUser.id']).count()
+        df = df[df['text_count'] > 10000][
+            [
+                'fromUser.displayName',
+                'fromUser.username',
+                'fromUser.id',
+                'mentions',
+                'urls',
+                'readBy',
+                'editedAt',
+                'sent',
+                'id',
+                'text'
+            ]
+        ]
+        # print(df)  # TODO: use logger
 
-    # serializes the dataframe in json file
-    with open(in_name, 'w') as file:
-        json.dump(df.to_json(orient='records'), file, indent=1)
-    print('Success!')  # TODO: use Logger
+        # serializes the dataframe in json file
+        df.to_json(orient='records', path_or_buf=out_name)
+        print('Success!')  # TODO: use Logger
+    except Exception as e:
+        print(e)
+        breakpoint()
 
 
 if __name__ == '__main__':
