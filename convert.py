d649771a3c20da30a3fe773f415240ecc2772c8c
WinstonChenn <904892813@qq.cpm>
2019-05-11 19:21:29 -0700

    win: update convert.py replace sort_data.py but with the same functionality

---
+++ convert.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import json
+
+
+"""
+this part of the code takes in the original csv, filter and sort a
+list of users who send more than 10000 messages and print out a
+dataframe with messages from users in that list, and dataframe only
+contain columns:'fromUser.displayName', 'fromUser.id', 'mentions',
+'readBy', 'sent', 'text'
+"""
+df = pd.read_csv('freecodecamp_casual_chatroom.csv')
+df_filtered = df[['fromUser.displayName', 'text']]
+rank = df_filtered.groupby(['fromUser.displayName']).count()
+rank = rank[rank['text'] > 10000]
+sort_rank = rank.sort_values(ascending=False, by='text')
+print(sort_rank)
+name_list = sort_rank.index.tolist()
+print(name_list, len(name_list))
+df = df[df['fromUser.displayName'].isin(name_list)]
+col_list = ['fromUser.displayName', 'fromUser.id', 'mentions',
+            'readBy', 'sent', 'text']
+df = df[col_list]
+print(df)
+
+"""
+this part convert filtered dataframe into json object and save json
+object into json file
+"""
+with open('data.json', 'w') as js_file:
+    js = df.to_json(orient='records')
+    json.dump(js, js_file)
