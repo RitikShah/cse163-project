31f5df2bdcff7e817c33cc8a435847f1a023e5c7
WinstonChenn <904892813@qq.cpm>
2019-05-28 10:01:11 -0700

    win: make clean data function a method and add a new get_features function basically add sentiment to datafram

--- clean_data.py
+++ clean_data.py
@@ -1,27 +1,11 @@
 import pandas as pd
 import re
 import string
-
-# drop empty text
-df = pd.read_csv('Data/freecodecamp_casual_chatroom.csv', na_values=None,
-                 low_memory=False)
-df = df.loc[0:10]
-df = df.dropna(subset=['text'])
-# select certain columns
-df = df[[
-            'fromUser.displayName',
-            'fromUser.username',
-            'fromUser.id',
-            'mentions',
-            'urls',
-            'readBy',
-            'editedAt',
-            'id',
-            'text'
-        ]]
+from sklearn.feature_extraction.text import CountVectorizer
+from textblob import TextBlob
 
 
-def clean_data(sentence):
+def clean_sentence(sentence):
     result = sentence.lower()
     result = re.sub('\[!@#$%^&().*?\:"<>~+=]', '', result)
     result = re.sub('[%s]' % re.escape(string.punctuation), '', result)
@@ -29,5 +13,69 @@ def clean_data(sentence):
     return result
 
 
-df['text'] = df['text'].apply(clean_data)
-print(df)
+def clean1(file):
+    # drop empty text
+    df = pd.read_csv(file, na_values=None, low_memory=False)
+    df = df.dropna(subset=['text'])
+    # select certain columns
+    df = df[[
+                'fromUser.displayName',
+                'fromUser.username',
+                'fromUser.id',
+                'mentions',
+                'urls',
+                'readBy', 
+                'editedAt',
+                'id',
+                'text'
+            ]]
+    df.to_pickle('corpus.pkl')
+    df['text'] = df['text'].apply(clean_sentence)
+    df.to_pickle('clean_data.pkl')
+
+
+def remove_common_words(pickle):
+    df = pd.read_pickle(pickle)
+    message_count = df.groupby(['fromUser.id']).count()
+    user_list = list(message_count[message_count['text'] > 100000].index)
+    df['text'] += " "
+    df = df[df['fromUser.id'].isin(user_list)]
+    user_df = pd.DataFrame(df.groupby('fromUser.username')['text'].sum())
+    cv = CountVectorizer(stop_words='english')
+    data_cv = cv.fit_transform(user_df.text)
+    data_dtm = pd.DataFrame(data_cv.toarray(), columns=cv.get_feature_names())
+    data_dtm.index = user_df.index
+    print(data_dtm)
+    """
+    top_dict = {}
+    for c in data_dtm.columns:
+        top = data_dtm[c].sort_values(ascending=False).head(30)
+        top_dict[c] = list(zip(top.index, top.values))
+    print(top_dict)
+    """
+
+
+def pol(x):
+    return TextBlob(x).sentiment.polarity
+
+
+def sub(x):
+    return TextBlob(x).sentiment.subjectivity
+
+
+def get_features(pickle):
+    df = pd.read_pickle(pickle)
+    df['polarity'] = df['text'].apply(pol)
+    df['subjectivity'] = df['text'].apply(sub)
+
+    df.to_pickle('featured_data.pkl')
+
+
+def main():
+    clean1('Data/freecodecamp_casual_chatroom.csv')
+    # remove_common_words('clean_data.pkl')
+    get_features('clean_data.pkl')
+
+
+if __name__ == '__main__':
+    main()
