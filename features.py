77e19c5265035a96f944fb1919e9968b59ab6414
WinstonChenn <904892813@qq.cpm>
2019-05-29 16:30:58 -0700

    win: make get_features function return a new dataframe only contain features and message ID

--- features.py
+++ features.py
@@ -1,34 +1,48 @@
 from textblob import TextBlob
+import pandas as pd
 
 
 def polarity(df):
     df['polarity'] = df['text_clean'].apply(
         lambda x: TextBlob(x).sentiment.polarity
     )
+    return df['polarity']
 
 
 def subjectivity(df):
     df['subjectivity'] = df['text_clean'].apply(
         lambda x: TextBlob(x).sentiment.subjectivity
     )
+    return df['subjectivity']
 
 
 def word_count(df):
     df['word_count'] = df['text_clean'].str.split().apply(len)
+    return df['word_count']
 
 
 def avg_word_length(df):
     df['ave_word_length'] = \
         df['text_clean'].str.replace(' ', '').apply(len) / df['word_count']
+    return df['ave_word_length']
 
 
 def get_features(df):
     # sentiment features
-    polarity(df)
-    subjectivity(df)
-
+    pol_series = polarity(df)
+    sub_series = subjectivity(df)
     # word count
-    word_count(df)
-
+    count_series = word_count(df)
     # average word length
-    avg_word_length(df)
+    length_series = avg_word_length(df)
+
+    data_dict = {'id': df['id'], pol_series.name: pol_series, sub_series.name: sub_series,
+                 count_series.name: count_series, length_series.name:
+                 length_series}
+    df = pd.DataFrame(data_dict)
+    print(df) # test code
+    df.to_pickle('feature_data.pkl')
+
+
+if __name__ == "__main__":
+    print(pd.read_pickle('feature_data.pkl'))
