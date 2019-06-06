715bf82686e7a47a60cd5f7ec3415b883607c04e
WinstonChenn <904892813@qq.cpm>
2019-06-06 16:27:24 -0700

    win: updarte ml test

--- machine_learning_test.py
+++ machine_learning_test.py
@@ -43,7 +43,7 @@ def isolated_test(train, dev, test):
             train_predict = model.predict(x_train)
             dev_predict = model.predict(x_test)
             train_score = mean_squared_error(y_train, train_predict)
-            dev_score = mean_squared_error(y_test, dev_predict)
+            dev_score = mean_squared_error(y_dev, dev_predict)
 
             # print('#:', i)
             # print('train_predict', train_predict)
@@ -73,164 +73,210 @@ def isolated_test(train, dev, test):
         # plt.savefig('max_depth.png')
 
     if ask_question('test min_samples_split? [Y or N]: '):
-        plot_data_2 = []
-        for i in range(1, 100):
-            i = i / 100
+        min_mse = None
+        min_model = None
+        min_depth = 0
+
+        for i in range(2, 100, 10):
             model = DecisionTreeRegressor(min_samples_split=i)
             model.fit(x_train, y_train)
 
-            train_score = mean_squared_error(y_train, model.predict(x_train))
-            test_score = mean_squared_error(y_test, model.predict(x_test))
+            train_predict = model.predict(x_train)
+            dev_predict = model.predict(x_test)
+            train_score = mean_squared_error(y_train, train_predict)
+            dev_score = mean_squared_error(y_dev, dev_predict)
 
-            plot_data_2.append({'min samples split': i, 'mean square error':
-                                train_score, 'predict type': 'trainning'})
-            plot_data_2.append({'min samples split': i, 'mean square error':
-                                test_score, 'predict type': 'testing'})
+            print({'max depth': i,
+                   'mse train': train_score,
+                   'mse test': dev_score})
 
-        plot_data_2 = pd.DataFrame(plot_data_2)
-        test_data = plot_data_2[plot_data_2['predict type'] == 'testing']
-        print(plot_data_2.loc[test_data['mean square error'].idxmin()])
+            if min_mse is None or dev_score < min_mse:
+                min_mse = dev_score
+                min_depth = i
+                min_model = model
 
-        sns.relplot(kind='line', x='min samples split', y='mean square error',
-                    hue='predict type', data=plot_data_2)
-        plt.savefig('min_samples_split.png')
+        print(f'dev mse: {min_mse}')
+        print(f'depth: {min_depth}')
+
+        test_score = mean_squared_error(y_test, min_model.predict(x_test))
+        print(f'test mse: {test_score}')
 
     if ask_question('test min_samples_leaf? [Y or N]: '):
-        plot_data_3 = []
-        for i in range(1, 51):
+        min_mse = None
+        min_model = None
+        min_depth = 0
+
+        for i in range(1, 51, 10):
             i = i / 100
             model = DecisionTreeRegressor(min_samples_leaf=i)
             model.fit(x_train, y_train)
 
-            train_score = mean_squared_error(y_train, model.predict(x_train))
-            test_score = mean_squared_error(y_test, model.predict(x_test))
+            train_predict = model.predict(x_train)
+            dev_predict = model.predict(x_test)
+            train_score = mean_squared_error(y_train, train_predict)
+            dev_score = mean_squared_error(y_dev, dev_predict)
 
-            plot_data_3.append({'min sample leaf': i, 'mean square error':
-                                train_score, 'predict type': 'trainning'})
-            plot_data_3.append({'min sample leaf': i, 'mean square error':
-                                test_score, 'predict type': 'testing'})
+            print({'max depth': i,
+                   'mse train': train_score,
+                   'mse test': dev_score})
 
-        plot_data_3 = pd.DataFrame(plot_data_3)
-        test_data = plot_data_3[plot_data_3['predict type'] == 'testing']
-        print(plot_data_3.loc[test_data['mean square error'].idxmin()])
+            if min_mse is None or dev_score < min_mse:
+                min_mse = dev_score
+                min_depth = i
+                min_model = model
 
-        sns.relplot(kind='line', x='min sample leaf', y='mean square error',
-                    hue='predict type', data=plot_data_3)
-        plt.savefig('min_samples_leaf.png')
+        print(f'dev mse: {min_mse}')
+        print(f'depth: {min_depth}')
+
+        test_score = mean_squared_error(y_test, min_model.predict(x_test))
+        print(f'test mse: {test_score}')
 
     if ask_question('test min_weight_fraction_leaf ? [Y or N]: '):
-        plot_data_4 = []
-        for i in range(1, 51):
-            i = i / 100
+        min_mse = None
+        min_model = None
+        min_depth = 0
+
+        for i in range(1, 51, 10):
+            i = i / 10
             model = DecisionTreeRegressor(min_weight_fraction_leaf=i)
             model.fit(x_train, y_train)
 
-            train_score = mean_squared_error(y_train, model.predict(x_train))
-            test_score = mean_squared_error(y_test, model.predict(x_test))
+            train_predict = model.predict(x_train)
+            dev_predict = model.predict(x_test)
+            train_score = mean_squared_error(y_train, train_predict)
+            dev_score = mean_squared_error(y_dev, dev_predict)
 
-            plot_data_4.append({'min weight fraction leaf': i, 'mean square error':
-                                train_score, 'predict type': 'trainning'})
-            plot_data_4.append({'min weight fraction leaf': i, 'mean square error':
-                                test_score, 'predict type': 'testing'})
+            print({'max depth': i,
+                   'mse train': train_score,
+                   'mse test': dev_score})
 
-        plot_data_4 = pd.DataFrame(plot_data_4)
-        test_data = plot_data_4[plot_data_4['predict type'] == 'testing']
-        print(plot_data_4.loc[test_data['mean square error'].idxmin()])
+            if min_mse is None or dev_score < min_mse:
+                min_mse = dev_score
+                min_depth = i
+                min_model = model
 
-        sns.relplot(kind='line', x='min weight fraction leaf',
-                    y='mean square error', hue='predict type', data=plot_data_4)
-        plt.savefig('min_weight_fraction_leaf.png')
+        print(f'dev mse: {min_mse}')
+        print(f'depth: {min_depth}')
+
+        test_score = mean_squared_error(y_test, min_model.predict(x_test))
+        print(f'test mse: {test_score}')
 
     if ask_question('test max_features? [Y or N]: '):
-        plot_data_5 = []
-        for i in range(1, 51):
-            i = i / 100
+        min_mse = None
+        min_model = None
+        min_depth = 0
+
+        for i in range(1, 51, 10):
+            i = i / 10
             model = DecisionTreeRegressor(max_features=i)
             model.fit(x_train, y_train)
 
-            train_score = mean_squared_error(y_train, model.predict(x_train))
-            test_score = mean_squared_error(y_test, model.predict(x_test))
+            train_predict = model.predict(x_train)
+            dev_predict = model.predict(x_test)
+            train_score = mean_squared_error(y_train, train_predict)
+            dev_score = mean_squared_error(y_dev, dev_predict)
 
-            plot_data_5.append({'max features': i, 'mean square error':
-                                train_score, 'predict type': 'trainning'})
-            plot_data_5.append({'max features': i, 'mean square error': test_score,
-                                'predict type': 'testing'})
+            print({'max depth': i,
+                   'mse train': train_score,
+                   'mse test': dev_score})
 
-        plot_data_5 = pd.DataFrame(plot_data_5)
-        test_data = plot_data_5[plot_data_5['predict type'] == 'testing']
-        print(plot_data_5.loc[test_data['mean square error'].idxmin()])
+            if min_mse is None or dev_score < min_mse:
+                min_mse = dev_score
+                min_depth = i
+                min_model = model
 
-        sns.relplot(kind='line', x='max features', y='mean square error',
-                    hue='predict type', data=plot_data_5)
-        plt.savefig('max_features.png')
+        print(f'dev mse: {min_mse}')
+        print(f'depth: {min_depth}')
+
+        test_score = mean_squared_error(y_test, min_model.predict(x_test))
+        print(f'test mse: {test_score}')
 
     if ask_question('test max_leaf_nodes? [Y or N]: '):
-        plot_data_6 = []
-        for i in range(2, 100):
-            model = DecisionTreeRegressor(max_leaf_nodes=i)
+        min_mse = None
+        min_model = None
+        min_depth = 0
+
+        for i in range(2, 100, 10):
+            model = DecisionTreeRegressor(max_leaf_nodest=i)
             model.fit(x_train, y_train)
 
-            train_score = mean_squared_error(y_train, model.predict(x_train))
-            test_score = mean_squared_error(y_test, model.predict(x_test))
+            train_predict = model.predict(x_train)
+            dev_predict = model.predict(x_test)
+            train_score = mean_squared_error(y_train, train_predict)
+            dev_score = mean_squared_error(y_dev, dev_predict)
 
-            plot_data_6.append({'max leaf nodes': i, 'mean square error':
-                                train_score, 'predict type': 'trainning'})
-            plot_data_6.append({'max leaf nodes': i, 'mean square error':
-                                test_score, 'predict type': 'testing'})
+            print({'max depth': i,
+                   'mse train': train_score,
+                   'mse test': dev_score})
 
-        plot_data_6 = pd.DataFrame(plot_data_6)
-        test_data = plot_data_6[plot_data_6['predict type'] == 'testing']
-        print(plot_data_6.loc[test_data['mean square error'].idxmin()])
+            if min_mse is None or dev_score < min_mse:
+                min_mse = dev_score
+                min_depth = i
+                min_model = model
 
-        sns.relplot(kind='line', x='max leaf nodes', y='mean square error',
-                    hue='predict type', data=plot_data_6)
-        plt.savefig('max_leaf_nodes.png')
+        print(f'dev mse: {min_mse}')
+        print(f'depth: {min_depth}')
+
+        test_score = mean_squared_error(y_test, min_model.predict(x_test))
+        print(f'test mse: {test_score}')
 
     if ask_question('test min_impurity_decrease? [Y or N]: '):
-        plot_data_7 = []
-        for i in range(1, 100):
-            i = i / 100
+        min_mse = None
+        min_model = None
+        min_depth = 0
+
+        for i in range(1, 100, 10):
             model = DecisionTreeRegressor(min_impurity_decrease=i)
             model.fit(x_train, y_train)
 
-            train_score = mean_squared_error(y_train, model.predict(x_train))
-            test_score = mean_squared_error(y_test, model.predict(x_test))
+            train_predict = model.predict(x_train)
+            dev_predict = model.predict(x_test)
+            train_score = mean_squared_error(y_train, train_predict)
+            dev_score = mean_squared_error(y_dev, dev_predict)
 
-            plot_data_7.append({'min impurity decrease': i, 'mean square error':
-                                train_score, 'predict type': 'trainning'})
-            plot_data_7.append({'min impurity decrease': i, 'mean square error':
-                                test_score, 'predict type': 'testing'})
+            print({'max depth': i,
+                   'mse train': train_score,
+                   'mse test': dev_score})
 
-        plot_data_7 = pd.DataFrame(plot_data_7)
-        test_data = plot_data_7[plot_data_7['predict type'] == 'testing']
-        print(plot_data_7.loc[test_data['mean square error'].idxmin()])
+            if min_mse is None or dev_score < min_mse:
+                min_mse = dev_score
+                min_depth = i
+                min_model = model
 
-        sns.relplot(kind='line', x='min impurity decrease', y='mean square error',
-                    hue='predict type', data=plot_data_7)
-        plt.savefig('min_inpurity_decrease.png')
+        print(f'dev mse: {min_mse}')
+        print(f'depth: {min_depth}')
+
+        test_score = mean_squared_error(y_test, min_model.predict(x_test))
+        print(f'test mse: {test_score}')
 
     if ask_question('test min_impurity_split? [Y or N]: '):
-        plot_data_8 = []
-        for i in range(1, 100):
-            i = i / 100
+        min_mse = None
+        min_model = None
+        min_depth = 0
+
+        for i in range(1, 100, 10):
             model = DecisionTreeRegressor(min_impurity_split=i)
             model.fit(x_train, y_train)
 
-            train_score = mean_squared_error(y_train, model.predict(x_train))
-            test_score = mean_squared_error(y_test, model.predict(x_test))
+            train_predict = model.predict(x_train)
+            dev_predict = model.predict(x_test)
+            train_score = mean_squared_error(y_train, train_predict)
+            dev_score = mean_squared_error(y_dev, dev_predict)
 
-            plot_data_8.append({'min impurity split': i, 'mean square error':
-                                train_score, 'predict type': 'trainning'})
-            plot_data_8.append({'min impurity split': i, 'mean square error':
-                                test_score, 'predict type': 'testing'})
+            print({'max depth': i,
+                   'mse train': train_score,
+                   'mse test': dev_score})
 
-        plot_data_8 = pd.DataFrame(plot_data_8)
-        test_data = plot_data_8[plot_data_8['predict type'] == 'testing']
-        print(plot_data_8.loc[test_data['mean square error'].idxmin()])
+            if min_mse is None or dev_score < min_mse:
+                min_mse = dev_score
+                min_depth = i
+                min_model = model
 
-        sns.relplot(kind='line', x='min impurity split', y='mean square error',
-                    hue='predict type', data=plot_data_8)
-        plt.savefig('min_impurity_split.png')
+        print(f'dev mse: {min_mse}')
+        print(f'depth: {min_depth}')
+
+        test_score = mean_squared_error(y_test, min_model.predict(x_test))
+        print(f'test mse: {test_score}')
 
 
 def focused_test(pickle):
