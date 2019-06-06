ea8616fb5d1577d925a4488520f06a1ab5c5c770
WinstonChenn <904892813@qq.cpm>
2019-06-06 15:03:45 -0700

    win:merge

--- machine_learning.py
+++ machine_learning.py
@@ -8,17 +8,6 @@ logging.basicConfig(level=logging.DEBUG)
 # TODO: throw this in a function pls
 
 # hyper parameters constants
-<<<<<<< HEAD
-DEPTH = 6
-SAMPLES_SPLIT = 0.04
-SAMPLES_LEAF = 0.01
-WEIGHT_FRACTION_LEAF = 0.01
-LEAF_NODES = 42
-FEATURES = 0.26
-IMPURITY_DECREASE = 0.01
-
-data = pd.read_pickle('featured.pkl')
-=======
 DEPTH = 5
 SAMPLES_SPLIT = 0.13
 SAMPLES_LEAF = 0.02
@@ -29,17 +18,13 @@ IMPURITY_DECREASE = 0.03
 logging.info('unpickling')
 data = pd.read_pickle('featured.pkl')
 logging.info('dividing columns')
->>>>>>> 1d87e121461543e655c0650d0a3f0ab6112bd288
 ml_data = data = data.loc[:, data.columns != 'id']
 x = ml_data.loc[:, ml_data.columns != 'readBy']
 y = ml_data['readBy']
 logging.info('splitting data: test/train')
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-<<<<<<< HEAD
-=======
 logging.info('creating regressor')
->>>>>>> 1d87e121461543e655c0650d0a3f0ab6112bd288
 # hyper parameters were tested in an earlier program
 model = DecisionTreeRegressor(
     max_depth=DEPTH,
@@ -50,11 +35,8 @@ model = DecisionTreeRegressor(
     min_impurity_decrease=IMPURITY_DECREASE,
     max_features=FEATURES
 )
-<<<<<<< HEAD
-=======
 
 logging.info('fitting model')
->>>>>>> 1d87e121461543e655c0650d0a3f0ab6112bd288
 model.fit(x_train, y_train)
 
 logging.info('making predictions about test set')
