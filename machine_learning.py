32fad4d2f53a668e049fc82019eda6b013ece9e8
Ritik Shah <ritik10.shah@gmail.com>
2019-06-06 12:37:36 -0700

    rit: added logging to machine_learning

--- machine_learning.py
+++ machine_learning.py
@@ -2,7 +2,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
 import pandas as pd
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 # TODO: throw this in a function pls
 
 # hyper parameters constants
@@ -13,12 +15,16 @@ WEIGHT_FRACTION_LEAF = 0.02
 LEAF_NODES = 29
 IMPURITY_DECREASE = 0.03
 
-data = pd.read_pickle('data.pkl')
+logging.info('unpickling')
+data = pd.read_pickle('featured.pkl')
+logging.info('dividing columns')
 ml_data = data = data.loc[:, data.columns != 'id']
 x = ml_data.loc[:, ml_data.columns != 'readBy']
 y = ml_data['readBy']
+logging.info('splitting data: test/train')
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
+logging.info('creating regressor')
 # hyper parameters were tested in an earlier program
 model = DecisionTreeRegressor(
     max_depth=DEPTH,
@@ -29,11 +35,14 @@ model = DecisionTreeRegressor(
     min_impurity_decrease=IMPURITY_DECREASE
 )
 
+logging.info('fitting model')
 model.fit(x_train, y_train)
 
+logging.info('making predictions about test set')
 a = list(model.predict(x_test))
 b = list(y_test)
 
+logging.info('calculating error')
 close = 0
 for i in range(len(a)):
     print(b[i], a[i])
