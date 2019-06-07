950ea70335d45b46c9fbcbc968ea9c40ecf38513
WinstonChenn <904892813@qq.cpm>
2019-06-06 21:59:04 -0700

    win: bring graph analysis back but when turn to second graph doesnt work, a lot of DEBUG pop up

--- machine_learning_test.py
+++ machine_learning_test.py
@@ -1,6 +1,8 @@
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
 from split import train, dev, test
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import logging
 
@@ -18,6 +20,197 @@ def remove_id(dset):
 
 def x_y(dset):
     return dset.loc[:, dset.columns != 'readBy'], dset['readBy']
+
+
+def graph_analysis(train, dev):
+    train = remove_id(train)
+    dev = remove_id(dev)
+    train_frac = dev.sample(frac=0.05)
+    dev_frac = dev.sample(frac=0.05)
+    x_train, y_train = x_y(train_frac)
+    x_dev, y_dev = x_y(dev_frac)
+
+    if ask_question('test max_depth? [Y or N]: '):
+        plot_data_1 = []
+        for i in range(1, 100):
+            model = DecisionTreeRegressor(max_depth=i)
+            model.fit(x_train, y_train)
+
+            train_score = mean_squared_error(y_train, model.predict(x_train))
+            test_score = mean_squared_error(y_dev, model.predict(x_dev))
+
+            plot_data_1.append({'min samples split': i, 'mean square error':
+                                train_score, 'predict type': 'trainning'})
+            plot_data_1.append({'min samples split': i, 'mean square error':
+                                test_score, 'predict type': 'testing'})
+
+        plot_data_1 = pd.DataFrame(plot_data_1)
+        test_data = plot_data_1[plot_data_1['predict type'] == 'testing']
+        print(plot_data_1.loc[test_data['mean square error'].idxmin()])
+
+        sns.relplot(kind='line', x='min samples split', y='mean square error',
+                    hue='predict type', data=plot_data_1)
+        plt.savefig('graphs/min_samples_split.png')
+
+    if ask_question('test min_samples_split? [Y or N]: '):
+        plot_data_2 = []
+        for i in range(1, 100):
+            i = i / 100
+            model = DecisionTreeRegressor(min_samples_split=i)
+            model.fit(x_train, y_train)
+
+            train_score = mean_squared_error(y_train, model.predict(x_train))
+            test_score = mean_squared_error(y_dev, model.predict(x_dev))
+
+            plot_data_2.append({'min samples split': i, 'mean square error':
+                                train_score, 'predict type': 'trainning'})
+            plot_data_2.append({'min samples split': i, 'mean square error':
+                                test_score, 'predict type': 'testing'})
+
+        plot_data_2 = pd.DataFrame(plot_data_2)
+        test_data = plot_data_2[plot_data_2['predict type'] == 'testing']
+        print(plot_data_2.loc[test_data['mean square error'].idxmin()])
+
+        sns.relplot(kind='line', x='min samples split', y='mean square error',
+                    hue='predict type', data=plot_data_2)
+        plt.savefig('graphs/min_samples_split.png')
+
+    if ask_question('test min_samples_leaf? [Y or N]: '):
+        plot_data_3 = []
+        for i in range(1, 51):
+            i = i / 100
+            model = DecisionTreeRegressor(min_samples_leaf=i)
+            model.fit(x_train, y_train)
+
+            train_score = mean_squared_error(y_train, model.predict(x_train))
+            test_score = mean_squared_error(y_dev, model.predict(x_dev))
+
+            plot_data_3.append({'min sample leaf': i, 'mean square error':
+                                train_score, 'predict type': 'trainning'})
+            plot_data_3.append({'min sample leaf': i, 'mean square error':
+                                test_score, 'predict type': 'testing'})
+
+        plot_data_3 = pd.DataFrame(plot_data_3)
+        test_data = plot_data_3[plot_data_3['predict type'] == 'testing']
+        print(plot_data_3.loc[test_data['mean square error'].idxmin()])
+
+        sns.relplot(kind='line', x='min sample leaf', y='mean square error',
+                    hue='predict type', data=plot_data_3)
+        plt.savefig('graphs/min_samples_leaf.png')
+
+    if ask_question('test min_weight_fraction_leaf ? [Y or N]: '):
+        plot_data_4 = []
+        for i in range(1, 51):
+            i = i / 100
+            model = DecisionTreeRegressor(min_weight_fraction_leaf=i)
+            model.fit(x_train, y_train)
+
+            train_score = mean_squared_error(y_train, model.predict(x_train))
+            test_score = mean_squared_error(y_dev, model.predict(x_dev))
+
+            plot_data_4.append({'min weight fraction leaf': i, 'mean square error':
+                                train_score, 'predict type': 'trainning'})
+            plot_data_4.append({'min weight fraction leaf': i, 'mean square error':
+                                test_score, 'predict type': 'testing'})
+
+        plot_data_4 = pd.DataFrame(plot_data_4)
+        test_data = plot_data_4[plot_data_4['predict type'] == 'testing']
+        print(plot_data_4.loc[test_data['mean square error'].idxmin()])
+
+        sns.relplot(kind='line', x='min weight fraction leaf',
+                    y='mean square error', hue='predict type', data=plot_data_4)
+        plt.savefig('graphs/min_weight_fraction_leaf.png')
+
+    if ask_question('test max_features? [Y or N]: '):
+        plot_data_5 = []
+        for i in range(1, 51):
+            i = i / 100
+            model = DecisionTreeRegressor(max_features=i)
+            model.fit(x_train, y_train)
+
+            train_score = mean_squared_error(y_train, model.predict(x_train))
+            test_score = mean_squared_error(y_dev, model.predict(x_dev))
+
+            plot_data_5.append({'max features': i, 'mean square error':
+                                train_score, 'predict type': 'trainning'})
+            plot_data_5.append({'max features': i, 'mean square error': test_score,
+                                'predict type': 'testing'})
+
+        plot_data_5 = pd.DataFrame(plot_data_5)
+        test_data = plot_data_5[plot_data_5['predict type'] == 'testing']
+        print(plot_data_5.loc[test_data['mean square error'].idxmin()])
+
+        sns.relplot(kind='line', x='max features', y='mean square error',
+                    hue='predict type', data=plot_data_5)
+        plt.savefig('graphs/max_features.png')
+
+    if ask_question('test max_leaf_nodes? [Y or N]: '):
+        plot_data_6 = []
+        for i in range(2, 100):
+            model = DecisionTreeRegressor(max_leaf_nodes=i)
+            model.fit(x_train, y_train)
+
+            train_score = mean_squared_error(y_train, model.predict(x_train))
+            test_score = mean_squared_error(y_dev, model.predict(x_dev))
+
+            plot_data_6.append({'max leaf nodes': i, 'mean square error':
+                                train_score, 'predict type': 'trainning'})
+            plot_data_6.append({'max leaf nodes': i, 'mean square error':
+                                test_score, 'predict type': 'testing'})
+
+        plot_data_6 = pd.DataFrame(plot_data_6)
+        test_data = plot_data_6[plot_data_6['predict type'] == 'testing']
+        print(plot_data_6.loc[test_data['mean square error'].idxmin()])
+
+        sns.relplot(kind='line', x='max leaf nodes', y='mean square error',
+                    hue='predict type', data=plot_data_6)
+        plt.savefig('graphs/max_leaf_nodes.png')
+
+    if ask_question('test min_impurity_decrease? [Y or N]: '):
+        plot_data_7 = []
+        for i in range(1, 100):
+            i = i / 100
+            model = DecisionTreeRegressor(min_impurity_decrease=i)
+            model.fit(x_train, y_train)
+
+            train_score = mean_squared_error(y_train, model.predict(x_train))
+            test_score = mean_squared_error(y_dev, model.predict(x_dev))
+
+            plot_data_7.append({'min impurity decrease': i, 'mean square error':
+                                train_score, 'predict type': 'trainning'})
+            plot_data_7.append({'min impurity decrease': i, 'mean square error':
+                                test_score, 'predict type': 'testing'})
+
+        plot_data_7 = pd.DataFrame(plot_data_7)
+        test_data = plot_data_7[plot_data_7['predict type'] == 'testing']
+        print(plot_data_7.loc[test_data['mean square error'].idxmin()])
+
+        sns.relplot(kind='line', x='min impurity decrease', y='mean square error',
+                    hue='predict type', data=plot_data_7)
+        plt.savefig('graphs/min_inpurity_decrease.png')
+
+    if ask_question('test min_impurity_split? [Y or N]: '):
+        plot_data_8 = []
+        for i in range(1, 100):
+            i = i / 100
+            model = DecisionTreeRegressor(min_impurity_split=i)
+            model.fit(x_train, y_train)
+
+            train_score = mean_squared_error(y_train, model.predict(x_train))
+            test_score = mean_squared_error(y_dev, model.predict(x_dev))
+
+            plot_data_8.append({'min impurity split': i, 'mean square error':
+                                train_score, 'predict type': 'trainning'})
+            plot_data_8.append({'min impurity split': i, 'mean square error':
+                                test_score, 'predict type': 'testing'})
+
+        plot_data_8 = pd.DataFrame(plot_data_8)
+        test_data = plot_data_8[plot_data_8['predict type'] == 'testing']
+        print(plot_data_8.loc[test_data['mean square error'].idxmin()])
+
+        sns.relplot(kind='line', x='min impurity split', y='mean square error',
+                    hue='predict type', data=plot_data_8)
+        plt.savefig('graphs/min_impurity_split.png')
 
 
 def depth(x_train, y_train, x_dev, y_dev, x_test, y_test):
@@ -314,6 +507,7 @@ def focused_test(train, dev, test):
 
 
 def main():
+    graph_analysis(train(), dev())
     # isolated_test(train(), dev(), test())
     focused_test(train(), dev(), test())
 
